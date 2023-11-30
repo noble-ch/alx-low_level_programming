a6d0c98aaa846a6ca908d8ac927229ff1447f03c
noble <birunoble@gmail.com>
2023-11-30 22:59:37 +0300

    debugging the first problem check 1 problems

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,26 +2,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * sum_them_all - sum all arguements
- * @n: number of arguements
- * Return: 0 is n is 0 or return sum
- */
 
+/**
+ * sum_them_all - sums all arguments passed
+ *
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list numbers;
-	int sum;
 	unsigned int i;
+	int sum;
+	va_list list;
 
-	if (n == 0)
-		return (0);
-	sum = 0;
-	va_start(numbers, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(numbers, int);
-	}
-	va_end(numbers);
+	va_start(list, n);
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
 	return (sum);
 }
