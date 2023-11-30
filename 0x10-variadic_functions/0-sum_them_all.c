2d33aff65bf084b370e7c9bf19e9c9845005105e
noble <birunoble@gmail.com>
2023-11-30 23:01:24 +0300

    debug the first question check 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,22 +4,25 @@
 
 
 /**
- * sum_them_all - sums all arguments passed
+ * sum_them_all - function with two arguments
+ * @n: int type first argume t
  *
- * @n: number of arguments
- *
- * Return: sum of arguments
+ * Description: returns the sum of all its parameters
+ * Return: value of sum or 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum;
-	va_list list;
+	int sum = 0;
+	va_list arg;
 
-	va_start(list, n);
-	for (i = 0, sum = 0; i < n; i++)
-		sum += va_arg(list, int);
-
-	va_end(list);
+	if (n == 0)
+		return (0);
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg, int);
+	}
+	va_end(arg);
 	return (sum);
 }
