e69185c4e90f48d2fbc166ef3d224f556ad89319
noble <birunoble@gmail.com>
2023-12-19 00:54:47 +0300

    solved

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,37 @@
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "holberton.h"
-
+#include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX
- * standard output.
- *
- * @filename: name of the file.
- * @letters: actual number of letters it could read and print.
- * Return: the actual number of letters it could read and print.
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, chars_read, chars_written;
+	int fd;
+	int i, y;
 	char *buf;
-
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	if (!buf)
 		return (0);
-	chars_read = read(fd, buf, letters);
-	if (chars_read == -1)
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
 		return (0);
-	chars_written = write(STDOUT_FILENO, buf, chars_read);
-	if (chars_written == -1)
-		return (0);
+	}
+	buf[i] = '\0';
 	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
 	free(buf);
-	return (chars_written);
+	return (y);
 }
